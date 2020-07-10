d577bd37b83e684f03981d18edac108a96c5c6a1
JackieBishop <jax.abert@gmail.com>
2020-07-10 12:35:29 -0400

    next

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,13 @@
-import shoppingList from './shopping-list.js';
+import $ from 'jquery';
+
+function main() {
+  console.log('DOM is loaded');
+
+  const startMsg = $('<p>Webpack is working!</p>');
+  $('#root').append(startMsg);
+}
+
+$(main);import shoppingList from './shopping-list.js';
 
 const main = function () {
   shoppingList.bindEventListeners();
@@ -6,3 +15,5 @@ const main = function () {
 };
 
 $(main);
+
+
